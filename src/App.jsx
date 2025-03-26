473f696430284b9c74eb73412f6252790db165c3
7248-om <joshiom564@gmail.com>
2025-03-26 18:09:52 +0530

    VoteCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react'
-
 import Navbar from './Navbar'
-
+import CustomVideoComponent from './VoteCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <title>Prontyl</title>
    <Navbar/>
     <div className="container w-screen h-screen flex justify-start items-end">
-      
+      <button onClick={() => setCount(count + 1)} className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full">tap</button>
+        Increment
 
       </div>
-    
+    <CustomVideoComponent/>
    </>
   )
 }
---
+++ src/VoteCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import voteVideo from './assets/vote.mp4';
+const CustomVideoComponent = () => {
+  return (
+    <div className="flex items-center justify-center p-6 m-25 bg-gray-100 rounded-xl ">
+      <div className="w-full md:w-1/2">
+        <h2 className="text-2xl font-bold mb-4">
+          Discover the power of Active Noise Cancellation in a film starring Pedro Pascal.
+        </h2>
+      </div>
+      <div className="ml-6 w-full md:w-1/2">
+        <video
+          controls
+          className="w-full h-auto rounded-lg shadow-lg"
+          poster="path_to_your_poster_image_here"
+          autoPlay
+          loop
+          muted
+        >
+          <source src={voteVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default CustomVideoComponent;
+
