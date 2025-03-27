f7e4b7e4d06f818d7eed7c4f779b6323df00b997
Nihar Mehta <nkmehta2162@gmail.com>
2025-03-27 18:10:33 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Hero from './components/Hero';
-import Navbar from './Navbar';
-import CustomVideoComponent from './VoteCard';
-import VotingMachineServices from './FaceCard';
-import About from './About Us'; 
-import Elections from './Elections'; 
-import Register from './Register'; 
-
+import Hero from './components/Hero'
+import Navbar from './Navbar'
+import CustomVideoComponent from './VoteCard'
+import VotingMachineServices from './FaceCard'
 function App() {
     return (
-        <Router>
+        <>
+            <title>Prontyl</title>
             <Navbar />
-            <Routes>
-                
-                <Route
-                    path="/"
-                    element={
-                        <>
-                            <Hero />
-                            <CustomVideoComponent />
-                            <VotingMachineServices />
-                        </>
-                    }
-                />
-               
-                <Route path="/elections" element={<Elections />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/about-us" element={<About />} />
-            </Routes>
-        </Router>
-    );
+            
+                <Hero />
+            
+            <CustomVideoComponent />
+            <VotingMachineServices/>
+        </>
+    )
 }
 
-export default App;
+export default App
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import prontylLogo from './assets/prontyl.jpeg'; // Ensure the image is in the same directory or adjust the path accordingly
 
 export default function Navbar() {
   return (
-    <nav className="fixed w-full bg-black z-50">
+    <nav className="fixed w-full bg-black  z-50 ">
+      {/* <div className="flex items-center"> */}
+      <img src={prontylLogo} alt="Prontyl Logo" className="h-10 w-auto absolute left-50 top-2" />
+        {/* </div> */}
       <div className="navbar__container flex justify-center items-center px-4 py-4">
-        <ul className="navbar__list flex space-x-10 text-white font-bold text-xl">
+        <ul className="navbar__list flex space-x-10 text-{white} font-bold text-xl">
           <li className="navbar__item">
-            <Link to="/" className="navbar__link hover:text-blue-500">
+            <Link to="/" className="navbar__link text-amber-50 hover:text-blue-500">
               Home
             </Link>
           </li>
           <li className="navbar__item">
-            <Link to="/elections" className="navbar__link hover:text-blue-500">
+            <Link to="/about" className="navbar__link text-amber-50 hover:text-blue-500">
               Elections
             </Link>
           </li>
           <li className="navbar__item">
-            <Link to="/register" className="navbar__link hover:text-blue-500">
+            <Link to="/services" className="navbar__link text-amber-50 hover:text-blue-500">
               Register
             </Link>
           </li>
           <li className="navbar__item">
-            <Link to="/about-us" className="navbar__link hover:text-blue-500">
+            <Link to="/about-us" className="navbar__link text-amber-50 hover:text-blue-500">
               About Us
             </Link>
           </li>
