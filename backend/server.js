866a3e79d74422e99f3f3410558f919bd705165c
Nihar Mehta <nkmehta2162@gmail.com>
2025-04-10 14:08:47 +0530

    73

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const cors = require('cors');
 const corsOptions = {
-    origin: 'http://localhost:5175', 
+    origin: 'http://localhost:5173', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
    
